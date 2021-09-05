c8461831d93b1547ed42d77641b58982f9e091c4
Eugene Starchenko <eugene.starchenko@gmail.com>
2021-09-05 12:42:16 +0300

    syntax

--- infra/pulumi/__main__.py
+++ infra/pulumi/__main__.py
@@ -12,9 +12,9 @@ profile_name = os.getenv("AWS_PROFILE")
 
 # Create an AWS provider instance using the named profile creds
 # and current region.
-aws_provider = aws.Provider("aws-provider",
-                            profile=profile_name,
-                            region=aws.get_region().name)
+aws_provider = aws.Provider(
+    "aws-provider", profile=profile_name, region=aws.get_region().name
+)
 
 # Define the AWS provider credential opts to configure the cluster's
 # kubeconfig auth.
@@ -30,31 +30,23 @@ public_subnet_cidrs = ["172.31.0.0/20", "172.31.48.0/20"]
 avail_zones = ["us-east-1a", "us-east-1b"]
 
 # Create VPC for EKS Cluster
-vpc = aws.ec2.Vpc(
-	"eks-vpc",
-	cidr_block="172.31.0.0/16"
-)
+vpc = aws.ec2.Vpc("eks-vpc", cidr_block="172.31.0.0/16")
 
 igw = aws.ec2.InternetGateway(
-	"eks-igw",
-	vpc_id=vpc.id,
+    "eks-igw",
+    vpc_id=vpc.id,
 )
 
 route_table = aws.ec2.RouteTable(
-	"eks-route-table",
-	vpc_id=vpc.id,
-	routes=[
-		{
-			"cidr_block": "0.0.0.0/0",
-			"gateway_id": igw.id
-		}
-	]
+    "eks-route-table",
+    vpc_id=vpc.id,
+    routes=[{"cidr_block": "0.0.0.0/0", "gateway_id": igw.id}],
 )
 
 public_subnet_ids = []
 
 # Create public subnets that will be used for the AWS Load Balancer Controller
-for zone, public_subnet_cidr  in zip(avail_zones, public_subnet_cidrs):
+for zone, public_subnet_cidr in zip(avail_zones, public_subnet_cidrs):
     public_subnet = aws.ec2.Subnet(
         f"eks-public-subnet-{zone}",
         assign_ipv6_address_on_creation=False,
@@ -63,11 +55,11 @@ for zone, public_subnet_cidr  in zip(avail_zones, public_subnet_cidrs):
         cidr_block=public_subnet_cidr,
         availability_zone=zone,
         tags={
-	     			# Custom tags for subnets
+            # Custom tags for subnets
             "Name": f"eks-public-subnet-{zone}",
             cluster_tag: "owned",
             "kubernetes.io/role/elb": "1",
-        }
+        },
     )
 
     aws.ec2.RouteTableAssociation(
@@ -80,7 +72,7 @@ for zone, public_subnet_cidr  in zip(avail_zones, public_subnet_cidrs):
 # Create an EKS cluster.
 cluster = eks.Cluster(
     cluster_name,
-		name=cluster_name,
+    name=cluster_name,
     vpc_id=vpc.id,
     instance_type="t2.medium",
     desired_capacity=1,
@@ -98,7 +90,9 @@ pulumi.export("kubeconfig", cluster.kubeconfig)
 # AWS Load Balancer Controller
 
 aws_lb_ns = "aws-lb-controller"
-service_account_name = f"system:serviceaccount:{aws_lb_ns}:aws-lb-controller-serviceaccount"
+service_account_name = (
+    f"system:serviceaccount:{aws_lb_ns}:aws-lb-controller-serviceaccount"
+)
 oidc_arn = cluster.core.oidc_provider.arn
 oidc_url = cluster.core.oidc_provider.url
 
@@ -151,12 +145,12 @@ namespace = k8s.core.v1.Namespace(
         "name": aws_lb_ns,
         "labels": {
             "app.kubernetes.io/name": "aws-load-balancer-controller",
-        }
+        },
     },
     opts=pulumi.ResourceOptions(
         provider=provider,
         parent=provider,
-    )
+    ),
 )
 
 service_account = k8s.core.v1.ServiceAccount(
@@ -166,23 +160,25 @@ service_account = k8s.core.v1.ServiceAccount(
         "namespace": namespace.metadata["name"],
         "annotations": {
             "eks.amazonaws.com/role-arn": iam_role.arn.apply(lambda arn: arn)
-        }
-    }
+        },
+    },
 )
 
 # This transformation is needed to remove the status field from the CRD
 # otherwise the Chart fails to deploy
+
+
 def remove_status(obj, opts):
     if obj["kind"] == "CustomResourceDefinition":
         del obj["status"]
 
+
 k8s.helm.v3.Chart(
-    "lb", k8s.helm.v3.ChartOpts(
+    "lb",
+    k8s.helm.v3.ChartOpts(
         chart="aws-load-balancer-controller",
         version="1.2.0",
-        fetch_opts=k8s.helm.v3.FetchOpts(
-            repo="https://aws.github.io/eks-charts"
-        ),
+        fetch_opts=k8s.helm.v3.FetchOpts(repo="https://aws.github.io/eks-charts"),
         namespace=namespace.metadata["name"],
         values={
             "region": "us-east-1",
@@ -192,15 +188,11 @@ k8s.helm.v3.Chart(
             },
             "vpcId": cluster.eks_cluster.vpc_config.vpc_id,
             "clusterName": cluster.eks_cluster.name,
-            "podLabels": {
-                "stack": pulumi.get_stack(),
-                "app": "aws-lb-controller"
-            }
+            "podLabels": {"stack": pulumi.get_stack(), "app": "aws-lb-controller"},
         },
-        transformations=[remove_status]
-    ), pulumi.ResourceOptions(
-        provider=provider, parent=namespace
-    )
+        transformations=[remove_status],
+    ),
+    pulumi.ResourceOptions(provider=provider, parent=namespace),
 )
 
 # Create private ECR repository
@@ -236,7 +228,7 @@ repo_policy = aws.ecr.RepositoryPolicy(
             }
         ]
     }""",
-    repository=ecr_repo.name
+    repository=ecr_repo.name,
 )
 
 ecr_access_policy = aws.iam.Policy(
@@ -255,7 +247,7 @@ ecr_access_policy = aws.iam.Policy(
           "Resource": "*"
         }
       ]
-    }"""
+    }""",
 )
 
 nodeInstanceRole = aws.iam.RolePolicyAttachment(
