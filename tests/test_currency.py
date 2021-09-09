0b494b2451049bc723832a9d6bb65d845c7e47f1
Eugene Starchenko <eugene.starchenko@gmail.com>
2021-09-09 16:12:02 +0300

    handle errors

--- tests/test_currency.py
+++ tests/test_currency.py
@@ -1,4 +1,9 @@
-def test_get_spot_prices(test_api):
+from fastapi import HTTPException
+
+from api.v1.endpoints import currency
+
+
+def test_get_spot_price(test_api):
     response = test_api.get("/v1/currency/")
     assert response.status_code == 200
 
@@ -8,7 +13,11 @@ def test_ticker(test_api):
     assert response.status_code == 200
 
 
-def test_invalid_ticker(test_api):
+def test_invalid_ticker(test_api, monkeypatch):
+    def mocked_response():
+        raise HTTPException(status_code=404, detail="Invalid currency")
+
+    monkeypatch.setattr(currency, "get_spot_prices", mocked_response)
     response = test_api.get("/v1/currency?ticker=XXX")
     assert response.status_code == 404
     assert response.json() == {"detail": "Invalid currency"}
